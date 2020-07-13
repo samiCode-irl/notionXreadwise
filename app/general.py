9317e320ca34fdfb14ad0c0882210c4382bea5ce
samiCode-irl <samimian0072@gmail.com>
2020-07-13 08:44:26 -0700

    Added Templates and Render Templates

--- app/general.py
+++ app/general.py
@@ -1,9 +1,21 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    return render_template('homepage.html')
 
-@app.route('/about')
-def about():
-    return 'About Us'
+# To show daily highlights
+@app.route('/daily')
+def today():
+    return render_template('today.html')
+
+# To show all the highlights in the web app database
+@app.route('/highlights')
+def highlights():
+    return render_template('highlights.html')
+
+# To add highlights into the webapp
+@app.route('/add')
+def add():
+    return render_template('add.html')
