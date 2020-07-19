bc2bcfc0db12338182108ca873ce78e88759f7b4
samiCode-irl <samimian0072@gmail.com>
2020-07-20 04:38:41 +0500

    Inital Development

--- app/__init__.py
+++ app/__init__.py
@@ -4,14 +4,14 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 app = Flask(__name__)
 
-ENV = 'dev'
+ENV = 'prod'
 
 if ENV == 'dev':
     app.debug = True
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 else:
     app.debug = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://nwwgtomfbspmhn:8dd03519fee4577a84867bbeb159dc5cf29dc31e327bede7a8344da04c1bf733@ec2-52-22-216-69.compute-1.amazonaws.com:5432/dabgen2a0p9bdp'
 
 app.config['SECRET_KEY'] = 'Super Secret Key'
 
